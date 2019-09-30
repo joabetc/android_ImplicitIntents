19387a96352c5c7e337ad93679b766a3f0debe45
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-09-30 14:08:03 -0300

    Implementing the Open Website button

--- app/src/main/java/com/joabe/implicitintents/MainActivity.java
+++ app/src/main/java/com/joabe/implicitintents/MainActivity.java
@@ -2,18 +2,35 @@ package com.joabe.implicitintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mWebsiteEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWebsiteEditText = findViewById(R.id.website_editText);
     }
 
     public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
+
+        Uri webPage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webPage);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
     }
 
     public void openLocation(View view) {
